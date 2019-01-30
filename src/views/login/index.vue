85c08aa9298ffc166090e13adf1e6a9c2f4e5cde
NewHms <37338418+NewHms@users.noreply.github.com>
2019-01-30 14:41:47 +0800

    修改登录页标题大小

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left"
              label-width="0px"
              class="card-box login-form">
-      <h3 class="title">自动脚本运行平台</h3>
+      <h2 class="title">自动脚本运行平台</h2>
       <el-form-item prop="username">
         <span class="svg-container svg-container_login">
           <svg-icon icon-class="user"/>
